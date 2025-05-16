9f25ad05d230755d3092414806710ecf7cdf9f7c
TheKazotskyKickGuy <89810421+TheKazotskyKickGuy@users.noreply.github.com>
2025-05-16 17:21:58 -0300

    Create Documento.java
    b

---
+++ Documento.java
@@ -0,0 +1,32 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Documento {
+    private String nomeArquivo;
+    private String usuario;
+    private LocalDateTime horarioSolicitacao;
+
+    public Documento(String nomeArquivo, String usuario) {
+        this.nomeArquivo = nomeArquivo;
+        this.usuario = usuario;
+        this.horarioSolicitacao = LocalDateTime.now();
+    }
+
+    public String getNomeArquivo() {
+        return nomeArquivo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public LocalDateTime getHorarioSolicitacao() {
+        return horarioSolicitacao;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return "Arquivo: " + nomeArquivo + ", Usuário: " + usuario + ", Solicitado às: " + horarioSolicitacao.format(fmt);
+    }
+}
